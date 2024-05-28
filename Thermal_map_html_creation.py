ff95b38e48da0c065a22f46b04ef1eee99d0ffd0
francorvalan <44734157+francorvalan@users.noreply.github.com>
2024-05-28 16:11:06 -0300

    Update Thermal_map_html_creation.py

--- Thermal_map_html_creation.py
+++ Thermal_map_html_creation.py
@@ -1,6 +1,6 @@
 from localtileserver import get_leaflet_tile_layer, TileClient
 from ipyleaflet import Map
-image_path = '../02_Data/02_Thermal/DJI_202402291328_007_PRUEBAPANELESSOLARES-30-50-80_RGB_3_bands_COG_jpeg.tif'
+image_path = 'Thermal_orthomosaic.tif'
 
 # First, create a tile server from local raster file
 tile_client = TileClient(image_path)
